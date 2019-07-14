6526fa7291ddad88356306c77de1e74721750c24
Kevin Phan <Kev.Phan15@gmail.com>
2019-07-14 16:31:43 -0700

    Finished oobjects and arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,65 @@
   object name, diet, weight, length, period
 */
 
+function Dino(attr) {
+  this.name = attr.name;
+  this.diet = attr.diet;
+  this.weight = attr.weight;
+  this.length = attr.length;
+  this.period = attr.period;
+};
+
+Dino.prototype.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!";
+};
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+
+const tyrannosaurus = new Dino({
+  name: 'Tyrannosaurus', 
+  diet: 'Carnivorous', 
+  weight: '7000kg', 
+  length: '12m', 
+  period: 'Late Cretaceious'
+});
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = new Dino({
+  name: 'Stegosaurus', 
+  diet: 'Herbivorous', 
+  weight: '2000kg', 
+  length: '9m', 
+  period: 'Late Jurassic'
+});
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor = new Dino({
+  name: 'Velociraptor', 
+  diet: 'Carnivorous', 
+  weight: '15kg', 
+  length: '1.8m', 
+  period: 'Late Cretaceious'
+});
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -50,7 +86,27 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (i=0; i<graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+function swap(array, i, j) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function alphaUni (array) {
+  for (i=0; i<array.length; i++) {
+    for (j=1; j < array.length; j++){
+      if (array[j-1] > array[j]) {
+        swap(array, j-1, j);
+      }
+    }
+  }
+  return array;
+};
+
+console.log(alphaUni(universities));
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -59,11 +115,23 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (i=0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+} 
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+let allUni = [];
+for (i=0; i < graduates.length; i++) {
+  allUni.push(graduates[i].university);
+}
+for (i=0; i < allUni.length; i++) {
+  if (allUni[i].search('Uni') > -1){
+    uni.push(allUni[i]);
+  }
+}
 console.log(uni);
 
 
